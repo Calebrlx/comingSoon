4846f60417eb70a8b1d61bc6eac3b3ecaf18f3be
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-03-25 16:12:26 -0800

    updates to data

--- data/data.ts
+++ data/data.ts
@@ -1,24 +1,24 @@
 const data = {
-    sitename: "Comming Soon Template",
-    sitetagline: "Comming Soon Template Tagline",
+    sitename: "Relix",
+    sitetagline: "AI meets innovation",
     sitelogo: "",
     title: "Comming Soon",
     description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
     newsletterheading: "Stay tuned for our live updates!",
-    copyrightText: 'Copyright © 2023 | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">Baikare Sandip</a>',
+    copyrightText: 'Copyright © 2023 | A product of &nbsp;<a target="_blank" class="no-underline md:underline" href="https://relix.ai">Relix Co</a>',
     socialIconsHeading: "Follow Us 📣",
     socialIcons: [
         {
             icon: "facebook",
-            link: "https://www.facebook.com/sandeep.baikare/",
+            link: "https://www.facebook.com/",
         },
         {
             icon: "twitter",
-            link: "https://twitter.com/BaikareSandeep",
+            link: "https://twitter.com/calebforestal",
         },
         {
             icon: "linkedIn",
-            link: "https://www.linkedin.com/in/sandip-baikare/",
+            link: "https://www.linkedin.com/in/calebforestal",
         },
     ],
 }
